077712cb3d2a2dd3d9d1a0eeaae2bc71b632e159
Adancurusul <1016867898@qq.com>
2020-09-07 14:03:24 +0800

    9.7

--- emulator/cr_tb.py
+++ emulator/cr_tb.py
@@ -9,6 +9,9 @@ def cr_convert():
     pc_next,branch_offset,r6_r7_data,cr_data = [Signal(intbv(0)[16:]) for _ in range (4)]
     cr_ins =cr( pc_next, branch_offset, r6_r7_data, cr_data,clk, rst, int0, int1, int2, int3, mem_read, mem_write, mem_ok, branch, selector, cr_write, ret, apc, jmp, bra , main_state)   #toVerilog(cr,pc_next, branch_offset, r6_r7_data, cr_data,clk, rst, int0, int1, int2, int3, mem_read, mem_write, mem_ok, branch, selector, cr_write, ret, apc, jmp, bra , main_state)
     return cr_ins
+def cr_sim():
+    crins = cr_convert()
+
 if __name__ == '__main__':
     cr_ins = cr_convert()
     cr_ins.convert(hdl='Verilog', initial_values=True)
